9792764960df7f1e0c027a4d6aaf32e53f8d8161
Manohar Chenna <manoharchenna@Manohars-MacBook-Air.local>
2024-09-16 11:00:03 +0530

    Changes for the second commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/general/home/home.component';
+import { PropertyComponent } from './pages/general/property/property.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent, },
+    { path: 'property', component: PropertyComponent, },
+];
+
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
