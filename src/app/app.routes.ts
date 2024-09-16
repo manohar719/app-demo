aed8c6d1caf47d50f6ce71e484436a81a849be6f
Manohar Chenna <manoharchenna@Manohars-MacBook-Air.local>
2024-09-16 12:18:26 +0530

    changes for the registry

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/general/home/home.component';
 import { PropertyComponent } from './pages/general/property/property.component';
+import { RegistryComponent } from './pages/general/registry/registry.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, },
     { path: 'property', component: PropertyComponent, },
+    { path: 'registry', component: RegistryComponent, },
 ];
 
 
